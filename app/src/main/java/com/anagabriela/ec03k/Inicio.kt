5b01bc95f67e84d59540ed5584ed585108d36e1d
AnaGabriela05 <avilchezsullon8@gmail.com>
2022-01-21 22:46:54 -0500

    add file

---
+++ app/src/main/java/com/anagabriela/ec03k/Inicio.kt
@@ -0,0 +1,11 @@
+package com.anagabriela.ec03k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_fragment)
+    }
+}
